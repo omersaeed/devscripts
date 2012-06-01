e6ac6b611fe1eba916fad35e95c9f5d0002aef68
Omer Saeed <omer.saeed@conradlabs.com>
2012-06-01 10:38:05 +0500

    Cleaned up script usage process

---
+++ myPaths.sh
@@ -0,0 +1,15 @@
+# This file should not be pushed, unless there are new parameters that get defined here that others need to add to their own version
+
+localRepos="bake, mesh, scheme, bastion, spire, auxl, dax, daft, enamel, bedrockjs, csi, gloss, lookandfeel, siq-vendor-js"
+upstreamRepos=""
+#upstreamRepos="bastion, spire, dax, auxl, daft"
+
+
+gitHome="/d/Projects/StoredIQ/git"
+gatewayHome="/d/Projects/StoredIQ/svn/siq_repo/gateway"
+
+#This is the path of the git repository home relative to gatewayHome
+projRPath="../../../git/"
+#This is the path of the svn siq_repo home relative to the git repo
+svnSiqRepoPath="../svn/siq_repo"
+
