1d2b760bd8135d273eb5ada4303e2c3cc83869a4
Muein Muzamil <muein.muzamil@conradlabs.com>
2012-07-12 12:31:17 +0500

    Updated scripts to include glad project

--- myPaths.sh
+++ myPaths.sh
@@ -1,6 +1,6 @@
 # This file should not be pushed, unless there are new parameters that get defined here that others need to add to their own version
 
-localRepos="bake, mesh, scheme, bastion, spire, auxl, dax, daft, enamel, bedrockjs, csi, gloss, lookandfeel, siq-vendor-js"
+localRepos="bake, mesh, scheme, bastion, spire, auxl, dax, daft, enamel, bedrockjs, csi, gloss, lookandfeel, siq-vendor-js, glad"
 upstreamRepos=""
 #upstreamRepos="bastion, spire, dax, auxl, daft"
 
