356961596e34337fa9b5b6866ff22b0347ceccbb
Omer Saeed <omer.saeed@conradlabs.com>
2012-11-30 11:51:37 +0500

    Testing

--- myPaths.sh
+++ myPaths.sh
@@ -1,6 +1,6 @@
 # This file should not be pushed, unless there are new parameters that get defined here that others need to add to their own version
 
-localRepos="bake, mesh, scheme, bastion, spire, auxl, dax, daft, enamel, bedrockjs, csi, gloss, lookandfeel, siq-vendor-js, glad"
+localRepos="bake, mesh, scheme, bastion, spire, auxl, dax, daft, enamel, bedrockjs, csi, gloss, lookandfeel, siq-vendor-js, glad, cloudiq, platoon, devscripts"
 upstreamRepos=""
 #upstreamRepos="bastion, spire, dax, auxl, daft, gloss, siq-vendor-js, glad, bedrockjs"
 
